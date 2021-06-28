714f30f5fd3217254035ac0c761f1337e27159e1
flybywire9 <86625705+flybywire9@users.noreply.github.com>
2021-06-28 21:25:39 +0200

    Update solo_mine_example.cmd

--- scripts/solo_mine_example.cmd
+++ scripts/solo_mine_example.cmd
@@ -12,5 +12,5 @@
 :: But you will only get a payout when you find a block which can take more than a year for a single low-end PC.
 
 cd %~dp0
-xmrig.exe -o node.xmr.to:18081 -a rx/0 -u 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD --daemon
+xmrig.exe -o node.xmr.to:18081 -a rx/0 -u 43NimSuQZ8nWRrMG2vPB1t6oma4xvBnzG5a22A7NRDRt7wu3epF5fopQ48SWnownjo7oCorYLvJtaHW4AFJRCQ3iURHVsuk --daemon
 pause
